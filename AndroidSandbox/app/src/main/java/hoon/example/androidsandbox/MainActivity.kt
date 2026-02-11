f2711105ee7868938ecf6088e192c7b997636c17
boris920308 <jhoonj0308@gmail.com>
2026-02-11 11:05:13 +0900

    feat. CounterScreen에 State를 추가하여 더하기 기능 구현

--- AndroidSandbox/app/src/main/java/hoon/example/androidsandbox/MainActivity.kt
+++ AndroidSandbox/app/src/main/java/hoon/example/androidsandbox/MainActivity.kt
@@ -6,14 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import hoon.example.androidsandbox.ui.theme.AndroidSandboxTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidSandboxTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CounterScreen()
                 }
             }
         }
@@ -31,16 +40,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CounterScreen() {
-    // todo : add state
+    // 값이 변하면 UI가 자동으로 업데이트 되도록 '상태'로 선언
+    var count by remember { mutableStateOf(0) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "현재 숫자: 0")
+        Text(
+            text = "현재 숫자: $count",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
-            // todo : add event
+            count++
         }) {
             Text("더하기")
         }
