2fb67a4f661f9afee530bca2fedb61adec5e1c41
boris920308 <jhoonj0308@gmail.com>
2026-02-11 10:56:52 +0900

    feat. CounterScreen ui 생성

--- AndroidSandbox/app/src/main/java/hoon/example/androidsandbox/MainActivity.kt
+++ AndroidSandbox/app/src/main/java/hoon/example/androidsandbox/MainActivity.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import hoon.example.androidsandbox.ui.theme.AndroidSandboxTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,5 +26,31 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun CounterScreen() {
+    // todo : add state
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "현재 숫자: 0")
+        Button(onClick = {
+            // todo : add event
+        }) {
+            Text("더하기")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CounterScreenPreview() {
+    AndroidSandboxTheme {
+        CounterScreen()
     }
 }
